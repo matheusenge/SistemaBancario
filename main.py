c5ff58fc25fe0770216699a7080c7f9a49d857a9
Matheus Enge <matheusenge@hotmail.com>
2023-01-06 16:59:30 -0300

    main

--- main.py
+++ main.py
@@ -14,44 +14,44 @@ contas: List[Conta] = []
 def main() -> None:
     while True:
         menu()
-        opcao: int = int(input('> '))
+        try:
+            opcao: int = int(input('> '))
+            match opcao:
+                case 1:
+                    dados: function = criar_conta(Cliente)
+                    cliente: Cliente = Cliente(dados.nome, dados.email, dados.cpf, dados.data_nascimento)
+                    conta: Conta = Conta(cliente)
+                    contas.append(conta)
+                    print(cliente)
+                    sleep(1)
 
-        match opcao:
-            case 1:
-                dados: function = criar_conta(Cliente)
-                cliente: Cliente = Cliente(dados.nome, dados.email, dados.cpf, dados.data_nascimento)
-                conta: Conta = Conta(cliente)
-                contas.append(conta)
-                print(cliente)
-                sleep(1)
+                case 2:
+                    listar_contas(contas)
 
-            case 2:
-                listar_contas(contas)
-                sleep(1)
+                case 3:
+                    atualizar_dados(contas)
 
-            case 3:
-                atualizar_dados(contas)
-                sleep(1)
+                case 4:
+                    excluir_conta(contas)
 
-            case 4:
-                excluir_conta(contas)
-                sleep(1)
+                case 5:
+                    metodo_deposito_ou_saque(contas)
 
-            case 5:
-                metodo_deposito_ou_saque(contas)
-                sleep(2)
-
-            case 6:
-                transferencia(contas)
-                sleep(2)
-            
-            case 0:
-                break
-            
-            case _:
-                print('Opção inválida.')
-                sleep(1.0)
-                continue
+                case 6:
+                    transferencia(contas)
+                
+                case 0:
+                    print('Obrigado! Até logo...')
+                    sleep(1)
+                    break
+                
+                case _:
+                    print('Opção inválida.')
+                    sleep(0.7)
+                    continue
+                
+        except ValueError:
+            print('Insira uma opção válida.')
 
 if __name__ == '__main__':
     main()
