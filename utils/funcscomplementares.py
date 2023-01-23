9252ae6e1df5d83187b6e681102c0cec09c19073
Matheus Enge <matheusenge@hotmail.com>
2023-01-23 19:42:48 -0300

    funções

--- utils/funcscomplementares.py
+++ utils/funcscomplementares.py
@@ -1,192 +1,164 @@
 
-from time import sleep
 import os
+from time import sleep
+
 from banco.conta import *
 from utils.validacoes import *
 
-def menu():
-    print('''
-- PRESSIONE 1 PARA CADASTRAR CLIENTE
-                                   
-- PRESSIONE 2 PARA LISTAR CLIENTES
-                                                            
-- PRESSIONE 3 PARA ATUALIZAR DADOS
 
-- PRESSIONE 4 PARA EXCLUIR CADASTRO
-                                            
-- PRESSIONE 5 PARA DEPOSITO/SAQUE
-                                              
-- PRESSIONE 6 PARA TRANSFERENCIA
-                                            
-- PRESSIONE 0 PARA SAIR DO SISTEMA
-    ''')
+def menu():
+    options = '''
+    1 - Cadastrar cliente
+    2 - Listar clientes
+    3 - Atualizar dados
+    4 - Excluir cadastro
+    5 - Depósito/Saque
+    6 - Transferência
+    0 - Sair do sistema
+    '''
+    print(options)
 
 
 def criar_conta(dadosUsuario):
     while True:
-        nome: str = input('Informe seu nome: ').title().strip()
-        valida = re.compile('[\W_]+')
-        if any(char.isdigit() for char in nome) or len(nome.strip()) < 2 or valida.search(nome) != None:
+        nome = input('Informe seu nome: ').title().strip()
+        if valida_nome(nome): break
+        else:
             print("Nome inválido.")
             continue
-        break
     
     while True:
-        email: str = input('Insira seu E-mail: ')
-        checa_email = valida_email(email)
-
-        if checa_email: break
-
-        else: 
+        email = input('Insira seu E-mail: ')
+        if valida_email(email): break
+        else:
             print('Insira um E-mail válido.')
             continue 
 
     while True:
-        cpf: str = input('Insira seu CPF: ')
-        checa_cpf = valida_cpf(cpf)
-        
-        if checa_cpf: break
-            
+        cpf = input('Insira seu CPF: ')
+        if valida_cpf(cpf): break
         else:
             print('CPF inválido.')
             continue
     
     while True:
-        data_nascimento: str = input('Informe sua data de nascimento com [/]: ')
-        data = valida_data(data_nascimento)
-        
-        if not data:
+        data_nascimento = input('Informe sua data de nascimento com [/]: ')
+        if valida_data(data_nascimento): break
+        else:
             print('Data de nascimento inválida.')
             continue
 
-        else: break
-
-    pessoa: object = dadosUsuario(nome, email, cpf, data_nascimento)
+    pessoa = dadosUsuario(nome, email, cpf, data_nascimento)
     print('Conta criada com sucesso!')
     return pessoa
 
 
 def listar_contas(contas):
-    if contas:
-        for conta in contas:
-            print(f'Agência: {conta.agencia}')
-            print(f'Nome: {conta.cliente.nome}')
-            print(f'E-mail: {conta.cliente.email}')
-            print(f'CPF: {conta.cliente.cpf}')
-            print(f'Data Nasc.: {conta.cliente.data_nascimento}')
-            print(f'Saldo: {formata_str_float(conta.saldo)}')
-            print(f'Limite: {formata_str_float(conta.limite)}')
-            print('-=' * 20)
-    else:
+    if not contas:
         senao()
+        return
+
+    for conta in contas:
+        print(f'Agência: {conta.agencia}')
+        print(f'Nome: {conta.cliente.nome}')
+        print(f'E-mail: {conta.cliente.email}')
+        print(f'CPF: {conta.cliente.cpf}')
+        print(f'Data Nasc.: {conta.cliente.data_nascimento}')
+        print(f'Saldo: {formata_str_float(conta.saldo)}')
+        print(f'Limite: {formata_str_float(conta.limite)}')
+        print('-=' * 20)
 
 
 def atualizar_dados(contas):
-    if contas:
-        agencia: int = int(input('Informe sua agência: '))
-        for conta in contas:
-            if agencia == conta.agencia: 
-                while True:
-                    novo_email: str = input('Digite seu novo E-mail: ')
-                    checa_email = valida_email(novo_email)
+    if not contas:
+        senao()
+        return
 
-                    if checa_email: break
-
-                    else:
-                        print('Insira um E-mail válido.')
-                        continue 
-                conta.cliente.email: str = novo_email
+    agencia = int(input('Informe sua agência: '))
+    conta_atualizar = next((conta for conta in contas if conta.agencia == agencia), None)
+    if conta_atualizar:
+        while True:
+            novo_email = input('Digite seu novo E-mail: ')
+            if valida_email(novo_email):
+                conta_atualizar.cliente.email = novo_email
                 print('E-mail atualizado com sucesso.')
-                sleep(1)
-                return
-        else:
-            print(f'Agência não encontrada. "{agencia}"')
-            return
-
-    senao()
+                break
+            else:
+                print('Insira um E-mail válido.')
+                continue 
+    else:
+        print(f'Agência "{agencia}" não encontrada.')
 
 
 def excluir_conta(contas):
-    if contas:
-        agencia: int = int(input('Informe sua agência: '))
-        for conta in contas:
-            if agencia == conta.agencia:
-                contas.pop()
-                print('Conta excluída com sucesso.')
-                return
-        else:
-            print(f'Não foi encontrada a agência "{agencia}"')
-            return
+    if not contas:
+        senao()
+        return
 
-    senao()
+    agencia = int(input('Informe a agência: '))
+    contas_a_excluir = [conta for conta in contas if conta.agencia == agencia]
+    if contas_a_excluir:
+        for conta in contas_a_excluir:
+            contas.remove(conta)
+        print('Conta excluída com sucesso.')
+    else:
+        print(f'Não foi encontrada a agência "{agencia}"')
 
 
 def metodo_deposito_saque(contas):
-    if contas:
-        escolha_metodo: int = int(input('Deseja realizar depósito(1) ou saque(2)?'))
-
-        match escolha_metodo:
-            case 1:   
-                agencia: int = int(input('Informe o número da sua agencia para depositar: '))
-                
-                for conta in contas:
-                    if agencia == conta.agencia:
-                        valor_deposito: float = float(input('Informe o valor: '))
-                        conta.depositar(valor_deposito)
-                        sleep(1)
-                        break
-                else:
-                    print(f'Não foi encontrada a agência "{agencia}"')
-                    sleep(1)
-                    os.system('cls')
-            case 2:      
-                agencia: int = int(input('Informe o número da sua agência para saque: '))
-         
-                for conta in contas:
-                    if agencia == conta.agencia:
-                        valor_saque: float = float(input('Informe o valor: '))
-                        conta.sacar(valor_saque)
-                        sleep(1)
-                        break
-                else:
-                    print(f'Não foi encontrada a agência "{agencia}"')
-                    sleep(1)
-                    os.system('cls')
-
-    else:
+    if not contas:
         senao()
+        return
+
+    escolha_metodo = int(input('Deseja realizar depósito(1) ou saque(2)?'))
+
+    if escolha_metodo == 1:
+        agencia = int(input('Informe o número da sua agência para depositar: '))
+        conta = next((conta for conta in contas if conta.agencia == agencia), None)
+        if conta:
+            valor_deposito = float(input('Informe o valor: '))
+            conta.depositar(valor_deposito)
+
+        else:
+            print(f'Não foi encontrada a agência "{agencia}"')
+    elif escolha_metodo == 2:
+        agencia = int(input('Informe o número da sua agência para saque: '))
+        conta = next((conta for conta in contas if conta.agencia == agencia), None)
+        if conta:
+            valor_saque = float(input('Informe o valor: '))
+            conta.sacar(valor_saque)
+
+        else:
+            print(f'Não foi encontrada a agência "{agencia}"')
+    else:
+        print("Entrada inválida")
         
 
-def depositar(valor, agencia, contas):
-    [conta.depositar(valor) for conta in contas if conta.agencia == agencia]
-
-
-def sacar(valor, agencia, contas):
-    [conta.sacar(valor) for conta in contas if conta.agencia == agencia]
-
-
 def transferencia(contas):
-    if contas:
-        agencia_saque: int = int(input('Informe a agência: '))
-        agencia_deposito: int = int(input('Informa a agência que deseja depositar: '))
-        valor: float = float(input('Valor que deseja depositar: '))
-
-    for conta in contas:
-        if agencia_saque == conta.agencia:
-            if conta.saldo >= valor:
-                sacar(valor, agencia_saque, contas)
-                depositar(valor, agencia_deposito, contas)
-                print('Transferência realizada com sucesso!')
-                break
-            else:
-                print('Você não tem saldo suficiente.')
-                break
-        else:
-            print('Agência não encontrada')
-            continue
-    else:
+    if not contas:
         senao()
+        return
 
+    agencia_saque = int(input('Informe a agência: '))
+    agencia_deposito = int(input('Informa a agência que deseja depositar: '))
+    valor = float(input('Valor que deseja depositar: '))
+
+    conta_saque = next((conta for conta in contas if conta.agencia == agencia_saque), None)
+    conta_deposito = next((conta for conta in contas if conta.agencia == agencia_deposito), None)
+    
+    if conta_saque and conta_deposito:
+        if conta_saque.saldo >= valor:
+            conta_saque.sacar(valor)
+            conta_deposito.depositar(valor)
+            print('Transferência realizada com sucesso!')
+        else:
+            print('Você não tem saldo suficiente.')
+    else:
+        if not conta_saque:
+            print(f'Agência de saque "{agencia_saque}" não encontrada')
+        if not conta_deposito:
+            print(f'Agência de deposito "{agencia_deposito}" não encontrada')
+    
 
 def senao():
     print('Nenhuma conta cadastrada.')
