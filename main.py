242a61e7d5390b03dd6c4ba8aff895247e849dde
Matheus Enge <matheusenge@hotmail.com>
2023-01-23 19:43:56 -0300

    main

--- main.py
+++ main.py
@@ -9,40 +9,44 @@ os.system('cls')
 
 contas: List[Conta] = []
 
-def main() -> None:
+def main():
     while True:
         menu()
-        opcao: str = (input('> '))
-        if opcao.isdigit():
-            opcao = int(opcao)
-            match opcao:
-                case 1:
-                    dados: function = criar_conta(Cliente)
-                    cliente: Cliente = Cliente(dados.nome, dados.email, dados.cpf, dados.data_nascimento)
-                    conta: Conta = Conta(cliente)
-                    contas.append(conta)
-                    sleep(1)
-
-                case 2: listar_contas(contas)
-
-                case 3: atualizar_dados(contas)
-
-                case 4: excluir_conta(contas)
-
-                case 5: metodo_deposito_saque(contas)
-
-                case 6: transferencia(contas)
-                
-                case 0:
-                    print('Obrigado! Até logo...')
-                    sleep(1)
-                    break
-                
-                case _:
-                    print('Opção inválida, tente novamente:')
-        else:
+        opcao = input('> ')
+        if not opcao.isdigit():
             print('Insira um número entre 1/6 - Sair do sistema [0].')
             sleep(1)
+            continue
+        opcao = int(opcao)
+        if opcao == 1:
+            dados = criar_conta(Cliente)
+            cliente = Cliente(dados.nome, dados.email, dados.cpf, dados.data_nascimento)
+            conta = Conta(cliente)
+            contas.append(conta)
+            sleep(1)
+
+        elif opcao == 2:
+            listar_contas(contas)
+
+        elif opcao == 3:
+            atualizar_dados(contas)
+
+        elif opcao == 4:
+            excluir_conta(contas)
+
+        elif opcao == 5:
+            metodo_deposito_saque(contas)
+
+        elif opcao == 6:
+            transferencia(contas)
+
+        elif opcao == 0:
+            print('Obrigado! Até logo...')
+            sleep(1)
+            break
+        
+        else:
+            print('Opção inválida, tente novamente:')
 
 
 if __name__ == '__main__': main()
