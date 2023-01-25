71791afd638c7c487cf18da19d6149e7d3b10451
Matheus Enge <matheusenge@hotmail.com>
2023-01-25 01:57:38 -0300

    validações

--- utils/validacoes.py
+++ utils/validacoes.py
@@ -1,54 +1,36 @@
 from datetime import datetime
 import re
-from typing import Union
+from typing import Union, Match, Pattern
 
-def valida_nome(nome):
-    try:
-        valida = re.compile('^[a-zA-Z0-9 ]+$')
-        if any(char.isdigit() for char in nome) or len(nome.strip()) < 2 or valida.search(nome) == None:
-            return False
-
-        return True
-    except ValueError: return False
+def valida_nome(nome: str) -> bool:
+    pattern: Pattern[str] = re.compile(r'^[a-zA-Z0-9 ]+$')
+    if any(char.isdigit() for char in nome) or len(nome.strip()) < 2 or pattern.search(nome) is None:
+        return False
+    return True
 
 
-def valida_email(email) -> str:
-    try:
-        if not re.match(r"^(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])$", email):
-            return False
+def valida_email(email: str) -> bool:
+    pattern: Pattern[str] = re.compile(r"^(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])$")
+    match: Match[str] = pattern.match(email)
+    if match is None:
+        return False
+    return True
 
-        return True
-    except ValueError: return False
 
+def valida_cpf(cpf: str) -> bool:
+    if not cpf.isdigit() or len(cpf) != 11 or all(digito == cpf[0] for digito in cpf):
+        return False
 
-def valida_cpf(cpf: str) -> Union[bool, str]:
-    try:
-        nove_digitos = cpf[:9]
-        digito_1 = get_cpf_digit(nove_digitos, 10)
-        digito_2 = get_cpf_digit(nove_digitos + str(digito_1), 11)
-        cpf_gerado_pelo_calculo = f'{nove_digitos}{digito_1}{digito_2}'
+    digitos = list(map(int, cpf))
+    check_digito_1 = (sum(digitos[i] * (10 - i) for i in range(9)) * 10 % 11) % 10
+    check_digito_2 = (sum(digitos[i] * (11 - i) for i in range(10)) * 10 % 11) % 10
 
-        if cpf == cpf_gerado_pelo_calculo:
-            return True
-        else: return False
-    except ValueError: return False
-    
-def get_cpf_digit(numero_cpf: str, contador: int) -> int:
-    result = 0
-    for digito in numero_cpf:
-        if digito.isdigit():
-            result += int(digito) * contador
-            contador -= 1
-    digito = (result * 10) % 11
-    return digito if digito <= 9 else 0
+    return check_digito_1 == digitos[9] and check_digito_2 == digitos[10]
 
 
 def valida_data(data: str) -> Union[datetime, str]:
-    try:
-        data_dt = datetime.strptime(data, "%d/%m/%Y")
-        
-        if data_dt > datetime.now():
-            return False
+    data_dt = datetime.strptime(data, "%d/%m/%Y")
+    if data_dt > datetime.now():
+        return False
+    return data_dt 
 
-        return data_dt 
-    except ValueError: return False
