17eb4f394d5ceb346965ab0ba163330a6db7106f
Matheus Enge <matheusenge@hotmail.com>
2023-02-15 15:12:47 -0300

    main

--- main.py
+++ main.py
@@ -1,18 +1,20 @@
 import os
-from typing import List, Callable
+from typing import Callable, List
+
 from banco.cliente import Cliente
 from banco.conta import Conta
-from utils.funcscomplementares import menu, criar_conta, listar_contas, atualizar_dados, excluir_conta, metodo_deposito_saque, transferencia
+from utils.funcscomplementares import *
+
 
 def main() -> None:
-    contas: List[Conta] = []
+    contas: List[Conta] = carregar_contas()
     os.system('cls' if os.name == 'nt' else 'clear')
     
     opcoes = {
         1: criar_conta,
-        2: listar_contas,
+        2: listar_cadastros,
         3: atualizar_dados,
-        4: excluir_conta,
+        4: excluir_cadastro,
         5: metodo_deposito_saque,
         6: transferencia,
         0: 'exit'
@@ -36,6 +38,7 @@ def main() -> None:
                 cliente = funcao(Cliente)
                 conta = Conta(cliente)
                 contas.append(conta)
+                salvar_contas(contas)
             else:
                 funcao(contas)
 
