0711b3b9f7643f7587dae1f5c8b4a7eba52385f5
Matheus Enge <matheusenge@hotmail.com>
2023-03-01 15:41:11 -0300

    funções

--- utils/funcscomplementares.py
+++ utils/funcscomplementares.py
@@ -75,26 +75,27 @@ def atualizar_dados(contas: List[Conta]) -> None:
         senao()
         return
 
-    try:
-        agencia = int(input('Informe sua agência: '))
-    except ValueError:
-        print("Por favor, informe um valor numérico para agência.")
-        return
+    while True:
+        try:
+            agencia = int(input('Informe sua agência: '))
+        except ValueError:
+            print("Por favor, informe um valor numérico para agência.")
+            continue
 
-    conta_atualizar = next((conta for conta in contas if conta.agencia == agencia), None)
-    if conta_atualizar:
-        while True:
-            novo_email = input('Digite seu novo E-mail: ')
-            if valida_email(novo_email):
-                conta_atualizar.cliente.email = novo_email
-                print('E-mail atualizado com sucesso.')
-                salvar_contas(contas)
-                break
-            else:
-                print('Insira um E-mail válido.')
-                continue 
-    else:
-        print(f'Agência "{agencia}" não encontrada.')
+        conta_atualizar = next((conta for conta in contas if conta.agencia == agencia), None)
+        if conta_atualizar:
+            while True:
+                novo_email = input('Digite seu novo E-mail: ')
+                if valida_email(novo_email):
+                    conta_atualizar.cliente.email = novo_email
+                    print('E-mail atualizado com sucesso.')
+                    salvar_contas(contas)
+                    break
+                else:
+                    print('Insira um E-mail válido.')
+                    continue 
+        else:
+            print(f'Agência "{agencia}" não encontrada.')
 
 
 def excluir_cadastro(contas: List[Conta]) -> None:
@@ -129,57 +130,115 @@ def metodo_deposito_saque(contas: List[Conta]) -> None:
         senao()
         return
 
-    escolha_metodo = int(input('Deseja realizar depósito(1) ou saque(2)?'))
+    while True:
+        try:
+            escolha_metodo = int(input('Deseja realizar depósito(1) ou saque(2)?'))
 
-    if escolha_metodo in [1, 2]:
-        agencia = int(input('Informe o número da sua agência: '))
-        conta = next((conta for conta in contas if conta.agencia == agencia), None)
-        if conta:
-            valor = float(input('Informe o valor: '))
-            if escolha_metodo == 1:
-                try:
-                    conta.depositar(valor)
-                    salvar_contas(contas)
-                except ValueError as e:
-                    print(f'Deposito falhou, {e}')
+            if escolha_metodo in [1, 2]:
+                agencia = int(input('Informe o número da sua agência: '))
+                conta = next((conta for conta in contas if conta.agencia == agencia), None)
+                if conta:
+                    valor = float(input('Informe o valor: '))
+                    if escolha_metodo == 1:
+                        try:
+                            conta.depositar(valor)
+                            salvar_contas(contas)
+                            break
+                        except ValueError as e:
+                            print(f'Deposito falhou, {e}')
+                    else:
+                        try:
+                            conta.sacar(valor)
+                            break
+                        except ValueError as e:
+                            print(f'Saque falhou, {e}')
+                else:
+                    print(f'Não foi encontrada a agência "{agencia}"')
             else:
-                try:
-                    conta.sacar(valor)
-                except ValueError as e:
-                    print(f'Saque falhou, {e}')
-        else:
-            print(f'Não foi encontrada a agência "{agencia}"')
-    else:
-        print("Opção inválida")
+                print("Opção inválida.")
+                continue
+        except ValueError as error:
+            print(f'Erro ao digitar valor: {error}')
+            continue
         
 
 def transferencia(contas: List[Conta]) -> None:
-    if not contas:
-        senao()
-        return
+        if len(contas) < 2:
+            print('Operação não pode ser realizada. É necessário ao menos 2 contas cadastradas.')
+            return
+        print('Transferência entre contas\n')
 
-    try:
-        agencia_saque = int(input('Informe a agência de saque: '))
-        agencia_deposito = int(input('Informe a agência de depósito: '))
-        valor = float(input('Valor que deseja transferir: '))
-    except ValueError:
-        print("Por favor, informe um valor numérico para agência e valor.")
-        return
+        while True:
+            numero_conta_origem = input('Digite o número da conta de origem: ')
+            if not numero_conta_origem.isdigit():
+                print('Número de conta inválido. Tente novamente.\n')
+                continue
+            numero_conta_origem = int(numero_conta_origem)
+            conta_saque = next((conta for conta in contas if conta.agencia == numero_conta_origem), None)
+            
+            if conta_saque is None:
+                print('Conta de origem não encontrada. Tente novamente.\n')
+                continue
+            for conta in contas:
+                if conta == conta_saque:
+                    print(f'\nConta de origem encontrada: {conta.cliente.nome} ({conta.agencia})')
+            break
 
-    conta_saque = next((conta for conta in contas if conta.agencia == agencia_saque), None)
-    conta_deposito = next((conta for conta in contas if conta.agencia == agencia_deposito), None)
-    
-    if not conta_saque:
-        print(f'Agência de saque "{agencia_saque}" não encontrada.')
-    elif not conta_deposito:
-        print(f'Agência de depósito "{agencia_deposito}" não encontrada.')
-    elif conta_saque.saldo < valor:
-        print('Você não tem saldo suficiente.')
-    else:
-        conta_saque.sacar(valor)
-        conta_deposito.depositar(valor)
-        print('Transferência realizada com sucesso!')
-        salvar_contas(contas)
+        while True:
+            numero_conta_destino = input('Digite o número da conta de destino: ')
+            if not numero_conta_destino.isdigit():
+                print('Número de conta inválido. Tente novamente.\n')
+                continue
+            numero_conta_destino = int(numero_conta_destino)
+            conta_deposito = next((conta for conta in contas if conta.agencia == numero_conta_destino), None)
+
+
+            if conta_deposito is None:
+                print('Conta de destino não encontrada. Tente novamente.\n')
+                continue
+            for conta in contas:
+                if conta == conta_deposito:
+                    print(f'\nConta de destino encontrada: {conta.cliente.nome} ({conta.agencia})')
+            break
+
+        while True:
+            try:
+                valor = float(input('Digite o valor a ser transferido: R$ '))
+                if valor <= 0:
+                    print('Valor deve ser maior que zero. Tente novamente.\n')
+                    continue
+                if conta_saque.saldo < valor:
+                    print('Saldo insuficiente para realizar a transferência. Tente novamente.\n')
+                    continue
+            except ValueError as error:
+                print(f'Erro ao digitar valor: {error}')
+                continue
+
+            confirma = confirmar_transferencia(conta_deposito)
+            if confirma:
+                conta_saque.sacar(valor)
+                conta_deposito.depositar(valor)
+                print(f'[{horas()}] Transferência realizada com sucesso!')
+                salvar_contas(contas)
+                break
+            else:
+                break
+
+
+def confirmar_transferencia(conta_deposito):
+    print(f'''Confirme os dados:\nAgência: {conta_deposito.agencia}\nNome destinatário: {conta_deposito.cliente.nome}\nCPF: {conta_deposito.cliente.cpf}\n''')
+    while True:
+        try:
+            confirmacao = int(input('Realizar transferência? [1]=sim /[2]=não '))
+            if confirmacao == 1:
+                return True
+            elif confirmacao == 2:
+                print('Transferência cancelada.')
+                return False
+            else:
+                raise ValueError('Opção inválida.')
+        except ValueError as error:
+            print(f'Erro ao confirmar transferência: {error}')
 
 
 def carregar_contas() -> List[Conta]:
